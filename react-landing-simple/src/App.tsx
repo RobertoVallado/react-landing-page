00b037ee18de3a5517d2a28f4754c3a039a646b4
CGC\rvallado <rvallado@ssmic.com>
2026-01-21 21:56:54 -0500

    ok , this should be a clean push to main

--- react-landing-simple/src/App.tsx
+++ react-landing-simple/src/App.tsx
@@ -6,7 +6,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from './components/Header';
 import Home from './components/Home';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
       <Footer />
